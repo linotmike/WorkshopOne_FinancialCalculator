05407cf089d953f349344749abb331e656681f7e
linotmike <128604219+linotmike@users.noreply.github.com>
2024-09-27 10:53:20 -0700

    Imported Scanner

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,12 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner();
     public static void main(String[] args) {
+
+
+
         System.out.println("Hello world!");
     }
 }
