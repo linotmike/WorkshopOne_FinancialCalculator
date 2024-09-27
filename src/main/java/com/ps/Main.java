c3c32ac6dab678098aa9ae7577d36b9be02d30da
linotmike <128604219+linotmike@users.noreply.github.com>
2024-09-27 12:07:07 -0700

    Added principalMethod

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,11 +2,27 @@ package com.ps;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner scanner = new Scanner();
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println("What is the principal amount?");
+        double principal = principalMethod();
 
 
 
-        System.out.println("Hello world!");
+
+
+//        System.out.println("Hello world!");
     }
+    public static double principalMethod() {
+        double response = scanner.nextDouble();
+        if(response > 0){
+            System.out.println("your principal is: " + response);
+        }else {
+            System.out.println("Please enter your principal");
+            return principalMethod();
+        }
+
+        return response;
+    }
+
 }
