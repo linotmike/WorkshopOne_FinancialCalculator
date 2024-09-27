34412dc3cb3a31972b4fbed6b1ecfbb94b238cef
linotmike <128604219+linotmike@users.noreply.github.com>
2024-09-27 10:39:04 -0700

    Initial commit

---
+++ src/main/java/com/ps/A.java
@@ -0,0 +1,9 @@
+package com.ps;
+
+echo "# WorkshopOne_FinancialCalculator" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/linotmike/WorkshopOne_FinancialCalculator.git
+git push -u origin main
