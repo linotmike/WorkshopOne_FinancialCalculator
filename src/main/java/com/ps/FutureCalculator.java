e4f000ddc6d28d546c2be770a62fe080b43eb47a
linotmike <128604219+linotmike@users.noreply.github.com>
2024-09-28 12:04:21 -0700

    Made a FutureCalculations class and added a deposit method

---
+++ src/main/java/com/ps/FutureCalculator.java
@@ -0,0 +1,30 @@
+package com.ps;
+import java.util.Scanner;
+
+
+public class FutureCalculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String [] args){
+        System.out.println("What is your name?");
+        String name  = scanner.nextLine();
+        System.out.println("What is the amount of your deposit?");
+        double deposit = depositMethod();
+
+
+    }
+
+
+    public static double depositMethod(){
+        int response = scanner.nextInt();
+
+        if(response > 0 ){
+            System.out.println("Your deposit is: " + response);
+            return response;
+        } else {
+            System.out.println("please enter your deposit");
+            return depositMethod();
+        }
+    }
+
+}
